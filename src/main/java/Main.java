398b223f9d07221f9dfa0e36ee030769f4c05d2e
Frince Nacion <louienacion9@gmail.com>
2026-01-18 22:07:28 +0800

    Implement echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner scanner;
+    static String input;
     static String command;
+    static String parameter;
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         scanner = new Scanner(System.in);
         boolean alive = true;
         do {
             System.out.print("$ ");
-            String command = scanner.nextLine();
+            input = scanner.nextLine();
+            String[] input_raw = input.split(" ", 2);
+            command = input_raw[0];
+            parameter = (input_raw.length != 1) ? input_raw[1]: "";
             switch (command) {
                 case "exit":
                     alive = false;
+                    break;
+                case "echo":
+                    System.out.println(parameter);
                     break;
                 default:
                     System.out.println(command + ": command not found");
