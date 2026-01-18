901ce0779215241ca75e81ed7a2d4ad34cda1aaf
Frince Nacion <louienacion9@gmail.com>
2026-01-18 21:09:48 +0800

    Handle invalid commands v.1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner;
+    static String command;
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
+        scanner = new Scanner(System.in);
         System.out.print("$ ");
-
+        String command = scanner.nextLine();
+        System.out.println(command + ": command not found");
     }
 }
