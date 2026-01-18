06e387f27735808998ff6c05bfc0877482040245
Frince Nacion <louienacion9@gmail.com>
2026-01-18 21:18:03 +0800

    REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         scanner = new Scanner(System.in);
-        System.out.print("$ ");
-        String command = scanner.nextLine();
-        System.out.println(command + ": command not found");
+        boolean alive = true;
+        do {
+            System.out.print("$ ");
+            String command = scanner.nextLine();
+            System.out.println(command + ": command not found");
+        }while(alive);
     }
 }
